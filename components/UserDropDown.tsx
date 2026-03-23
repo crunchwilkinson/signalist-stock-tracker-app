48b704d9d567301fe1e089b201e71ca42c565b19
Tristan Wilkinson <tristanwilkinson0803@gmail.com>
2026-03-23 17:58:14 +0200

    Testing the commit in Webstorm

---
+++ components/UserDropDown.tsx
@@ -0,0 +1,32 @@
+'use client';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {useRouter} from "next/navigation";
+
+const UserDropDown = () => {
+    const router = useRouter();
+    const handleSignOut = async () => {
+        await router.push('/sign-in');
+    }
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+export default UserDropDown
