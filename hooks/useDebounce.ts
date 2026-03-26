b38ebd64850ad6c4a9608a4f4389c8656c188fe5
Tristan Wilkinson <tristanwilkinson0803@gmail.com>
2026-03-26 10:59:11 +0200

    Implement Stock Search

---
+++ hooks/useDebounce.ts
@@ -0,0 +1,23 @@
+'use client';
+
+import { useCallback, useRef, useEffect } from 'react';
+
+export function useDebounce<T extends (...args: any[]) => void>(callback: T, delay: number) {
+    const callbackRef = useRef(callback);
+
+    useEffect(() => {
+        callbackRef.current = callback;
+    }, [callback]);
+
+    const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+    return useCallback((...args: Parameters<T>) => {
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
+
+        timeoutRef.current = setTimeout(() => {
+            callbackRef.current(...args);
+        }, delay);
+    }, [delay]);
+}
